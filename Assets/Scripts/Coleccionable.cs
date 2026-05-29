8c995bc97c781919629ebe8a9f8ce5110fbe23e5
Caro792 <carolina.sok@gmail.com>
2026-05-29 10:33:40 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/Scripts/Coleccionable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Coleccionable : MonoBehaviour
+{
+    
+    public static int contadorObjetos = 0;
+
+    private TextMeshProUGUI textoContador;
+
+    void Start()
+    {
+        
+        textoContador = GameObject.FindObjectOfType<TextMeshProUGUI>();
+        
+        if (textoContador != null && contadorObjetos == 0)
+        {
+            textoContador.text = "Objetos: 0";
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.transform.root.CompareTag("Player"))
+        {
+            contadorObjetos++;
+            
+            
+            Debug.Log("Objetos recolectados: " + contadorObjetos);
+
+            
+            if (textoContador != null)
+            {
+                textoContador.text = "Objetos: " + contadorObjetos;
+            }
+
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instancia;
+
+    public TextMeshProUGUI textoContador;
+    
+    
+    private static int contadorObjetos = 0; 
+
+    void Awake()
+    {
+        instancia = this;
+    }
+
+    public void SumarObjeto()
+    {
+        contadorObjetos++;
+        Debug.Log("Objetos recolectados: " + contadorObjetos);
+
+        if (textoContador != null)
+        {
+            textoContador.text = "Objetos: " + contadorObjetos;
+        }
+    }
+}
