1b4ea1798b79ab5f79236d4bfe5c520ac68a4ac6
Caro792 <carolina.sok@gmail.com>
2026-06-01 12:12:24 -0300

    Clase 3: HUD y Managers funcionales

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,24 +7,56 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instancia;
 
-    public TextMeshProUGUI textoContador;
+    [Header("Referencias de UI (TextMeshPro)")]
+    public TextMeshProUGUI textoContador;     
+    public TextMeshProUGUI textoTemporizador; 
     
-    
-    private static int contadorObjetos = 0; 
+    [Header("Variables del Juego")]
+    public float timer = 60f; 
+    private int contadorObjetos = 0; 
 
     void Awake()
     {
         instancia = this;
     }
 
-    public void SumarObjeto()
+    void Update()
     {
-        contadorObjetos++;
+       
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            
+            if (timer < 0) 
+            {
+                timer = 0; 
+            }
+
+            UpdateTimerText();
+        }
+    }
+
+    
+    public void UpdateScore(int scoreActualizado)
+    {
+        contadorObjetos = scoreActualizado; 
         Debug.Log("Objetos recolectados: " + contadorObjetos);
 
         if (textoContador != null)
         {
-            textoContador.text = "Objetos: " + contadorObjetos;
+           
+            textoContador.text = "Score: " + contadorObjetos;
+        }
+    }
+
+   
+    private void UpdateTimerText()
+    {
+        if (textoTemporizador != null)
+        {
+            int minutes = Mathf.FloorToInt(timer / 60f);
+            int seconds = Mathf.FloorToInt(timer % 60f);
+            textoTemporizador.text = string.Format("{0:00}:{1:00}", minutes, seconds);
         }
     }
 }
---
+++ Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+   
+    public int score = 0; 
+    private GameManager gameManager;
+
+    void Awake()
+    {
+        
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Coleccionable") || other.CompareTag("Collectible"))
+        {
+            score++; 
+            
+           
+            if (gameManager != null)
+            {
+                gameManager.UpdateScore(score);
+            }
+            
+            
+            Destroy(other.gameObject); 
+        }
+    }
+}
